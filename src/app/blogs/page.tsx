afccc5864320b7c77573a3cb5937365d2c8a7374
isalim079 <salimreza1025@gmail.com>
2024-09-18 21:27:15 +0600

    blogs page added

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,11 +2,13 @@ import BlogCard from "@/components/ui/BlogCard";
 import { Blog } from "@/types";
 
 const BlogsPage = async () => {
-  const res = await fetch("http://localhost:3004/blogs");
+  const res = await fetch("http://localhost:3004/blogs", {
+    cache: 'no-store'
+  });
   const blogs = await res.json();
 
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
         All Blogs From <span className="text-accent">Blogiz</span>
       </h1>
@@ -19,7 +21,7 @@ const BlogsPage = async () => {
           maxime illum? Quos!
         </i>
       </p>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4 my-5">
         {blogs.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
