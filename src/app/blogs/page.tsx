a1664432484f23e82b660a70e5a70b2faf6e5661
isalim079 <salimreza1025@gmail.com>
2024-09-22 16:45:13 +0600

    api route created in client side

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:3004/blogs", {
-    cache: 'no-store'
-  });
-  const blogs = await res.json();
+const BlogsPage =  () => {
+  // const res = await fetch("http://localhost:3004/blogs", {
+  //   cache: 'no-store'
+  // });
+  // const blogs = await res.json();
+
+  const {data: blogs, isLoading} = useGetBlogsQuery("")
 
   return (
     <div className="w-[90%] mx-auto">
@@ -22,7 +27,7 @@ const BlogsPage = async () => {
         </i>
       </p>
       <div className="grid grid-cols-3 gap-4 my-5">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
